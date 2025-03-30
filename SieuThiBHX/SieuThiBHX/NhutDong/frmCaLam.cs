d3d25823013387c768d6a11b238988775dad3b94
vonhutdong <vo.nhutdong.@gmail.com>
2025-03-30 12:49:59 +0700

    update design

--- SieuThiBHX/SieuThiBHX/NhutDong/frmCaLam.cs
+++ SieuThiBHX/SieuThiBHX/NhutDong/frmCaLam.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace SieuThiBHX
 {
-    public partial class Form1 : Form
+    public partial class frmCaLam : Form
     {
-        public Form1()
+        public frmCaLam()
         {
             InitializeComponent();
+        }
+
+        private void guna2GroupBox3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
